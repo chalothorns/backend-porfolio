e708048603512562bb614094dfcf690b01332008
chalothorns <chalot.loy@gmail.com>
2026-01-28 22:43:26 +0700

    edit link for linking vercel

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const corsOptions = {
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5175",
-        "https://chalothorn-portfolio.vercel.app/",
+        "https://chalothorn-portfolio.vercel.app",
     ],
 };
 
